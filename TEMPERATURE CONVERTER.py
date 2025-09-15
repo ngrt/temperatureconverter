df8f0028ebff64b92acc87915cbb90136c22dca8
ngrt <101061860+ngrt@users.noreply.github.com>
2025-09-15 21:09:27 +0300

    main

---
+++ TEMPERATURE CONVERTER.py
@@ -0,0 +1,4 @@
+first = str(input('Welcome to Temperature Converter V2! Type letter C (Celsius), F(Fahrenheit), K(Kelvin)'))
+second = str(input('Type second letter converting to!'))
+if first == second:
+    print("You've typed the same!")
